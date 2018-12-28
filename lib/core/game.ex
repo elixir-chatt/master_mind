d087fe0b314765fe11c9324f597af87bc7e8d8c1
Bruce Tate <bruce@rapidred.com>
2018-12-27 19:32:48 -0500

    functional core mastermind

--- lib/core/game.ex
+++ lib/core/game.ex
@@ -1,8 +1,47 @@
 defmodule MasterMind.Core.Game do
-  defstruct answer: [1, 2, 3, 4], guesses: []
-  def new(answer) do
-    %__MODULE__{ answer: answer }
+  defstruct [:answer, :guesses] 
+  def new(answer \\ nil) do
+    
+    %__MODULE__{ 
+      answer: random_answer(answer), 
+      guesses: [] 
+    }
   end
+  
+  def random_answer(nil) do
+    (1..4)
+    |> Enum.map( fn(_) -> :rand.uniform(6) end)
+  end
+  def random_answer(answer) do
+    answer
+  end
+  
+  def score(answer, guess) do
+    %{
+      reds: score_reds(answer, guess), 
+      whites: score_whites(answer, guess)
+    }
+  end
+  
+  def scores(game) do
+    game.guesses
+    |> Enum.map( fn(guess) -> score(game.answer, guess) end)
+  end
+  
+  def game_won?(game) do
+    game
+    |> scores
+    |> Enum.any?( &win?/1 )
+  end
+  
+  def game_lost?(game) do
+    length(game.guesses) >= 10 && !(game_won?(game))
+  end
+  
+  def win?(%{ red: 4 }), do: true
+  def win?(_), do: false
+  
+  
   
   def score_reds(answer, guess) do
     answer
@@ -23,5 +62,4 @@ defmodule MasterMind.Core.Game do
   def add_guess(%__MODULE__{}=game, guess) do
     %__MODULE__{ game | guesses: [guess|game.guesses]}
   end
-  
 end
